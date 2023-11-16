9647ba09c8615b778ac4b86768567ce22c2938e5
ricardorosa <rjrosa@outlook.com>
2023-11-16 09:16:50 +0000

    Committing AuthorIntegration tests

--- src/main/java/com/rr/bookApi/controllers/AuthorController.java
+++ src/main/java/com/rr/bookApi/controllers/AuthorController.java
@@ -4,6 +4,8 @@ import com.rr.bookApi.domain.dto.AuthorDTO;
 import com.rr.bookApi.domain.entities.AuthorEntity;
 import com.rr.bookApi.mappers.Mapper;
 import com.rr.bookApi.services.AuthorService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,9 +23,9 @@ public class AuthorController {
     }
 
     @PostMapping(path = "/authors")
-    public AuthorDTO createAuthor(@RequestBody AuthorDTO author){
+    public ResponseEntity<AuthorDTO> createAuthor(@RequestBody AuthorDTO author){
         AuthorEntity authorEntity = authorMapper.mapFrom(author);
         AuthorEntity savedAuthorEntity = authorService.createAuthor(authorEntity);
-        return authorMapper.mapTo(savedAuthorEntity);
+        return new ResponseEntity<>(authorMapper.mapTo(savedAuthorEntity), HttpStatus.CREATED);
     }
 }
